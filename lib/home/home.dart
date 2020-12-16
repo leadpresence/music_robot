a45d78959d9ee819fef5363cb9a2c95285f2630d
felix <noblefelix4@gmail.com>
2020-12-16 22:48:48 +0100

    starting

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+import '../utils/robot_colorsl.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('go'),
+    return Material(
+        color:RobotColors.primaryColor1,
+
+
     );
   }
 }
---
+++ lib/utils/robot_colorsl.dart
@@ -0,0 +1,9 @@
+import 'package:velocity_x/velocity_x.dart';
+import 'package:flutter/material.dart';
+
+mixin RobotColors {
+  static Color primaryColor1 = Vx.orange400;
+  static Color primaryColor2 = Vx.orange500;
+
+
+}
